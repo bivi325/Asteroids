06253fba2e1b62896515f200a29df707b22be08e
denys.havryliv <bivi325@gmail.com>
2020-03-24 23:55:01 +0200

    Added bullets. Added spatial conditions

--- asteroids.js
+++ asteroids.js
@@ -11,10 +11,11 @@ const newGame = "New Game";
 const newGamePosX = (canvas.width - (fontSize * newGame.length) / 2 - 20) / 2;
 const newGamePosY = (canvas.height + fontSize) / 2;
 let requestID;
-let score = 21000;
+let score = 0;
 
 const spaceship = {
   color: "white",
+  bulletColor: "Red",
   shape: [
     { x: canvas.height / 60, y: 0 },
     { x: -canvas.height / 60, y: -canvas.height / 80 },
@@ -26,8 +27,12 @@ const spaceship = {
     y: canvas.height / 2
   },
   angle: Math.floor(Math.random() * Math.floor(degToRad(360))),
-  speed: 3
+  speed: 3,
+  bulletsSpeed: 5,
+  lives: 3
 };
+
+const bullets = [];
 
 const commands = {};
 
@@ -42,7 +47,7 @@ function resizeCanvasToDisplaySize(canvas) {
   }
 }
 
-function textDraw(text, posX, posY, fontSize) {
+function drawText(text, posX, posY, fontSize) {
   ctx.font = `${fontSize}px Aria`;
   ctx.imageSmoothingEnabled = true;
   ctx.fillStyle = "#FFFFFF";
@@ -92,14 +97,24 @@ function drawSpaceship() {
   ctx.restore();
 }
 
-function livesDraw() {}
+function drawLives() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillText("Lives: " + spaceship.lives, canvas.width - 65, 20);
+}
 
-function spaceshipDraw() {
-  drawSpaceship();
+function drawBullets() {
+  for (bullet of bullets) {
+    ctx.beginPath();
+    ctx.arc(bullet.posX, bullet.posY, 2, 0, Math.PI * 2);
+    ctx.fillStyle = spaceship.bulletColor;
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 function loadGame() {
-  textDraw(newGame, newGamePosX, newGamePosY, fontSize);
+  drawText(newGame, newGamePosX, newGamePosY, fontSize);
   canvas.addEventListener("click", newGameMouseDown);
 }
 
@@ -112,7 +127,7 @@ function newGameMouseDown(e) {
   ) {
     if (relativeY > (canvas.height - fontSize) / 2 && relativeY < newGamePosY) {
       canvas.removeEventListener("click", newGameMouseDown);
-      canvas.style.cursor = 'none'
+      canvas.style.cursor = "none";
       startNewGame();
     }
   }
@@ -126,10 +141,19 @@ function keyDownHandler(event) {
   commands[event.key] = true;
 }
 
+function clickHandler() {
+  bullets.push({
+    posX: spaceship.position.x,
+    posY: spaceship.position.y,
+    angle: spaceship.angle
+  });
+}
+
 function startNewGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   document.addEventListener("keyup", keyUpHandler);
   document.addEventListener("keydown", keyDownHandler);
+  document.addEventListener("click", clickHandler);
   requestID = gameLoop();
 }
 
@@ -141,26 +165,50 @@ function gameLoop() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  textDraw(score, 10, 40, 30);
-  livesDraw();
-  spaceshipDraw();
+  drawText(score, 10, 40, 30);
+  drawLives();
+  drawSpaceship();
+  drawBullets();
 }
 
 function update() {
-  console.log(commands);
   if (commands["w"] == true) {
-    spaceship.position.x = spaceship.position.x + spaceship.speed * Math.cos(spaceship.angle);
-    spaceship.position.y = spaceship.position.y + spaceship.speed * Math.sin(spaceship.angle);
+    spaceship.position.x =
+      spaceship.position.x + spaceship.speed * Math.cos(spaceship.angle);
+    spaceship.position.y =
+      spaceship.position.y + spaceship.speed * Math.sin(spaceship.angle);
   }
   if (commands["s"] == true) {
-    spaceship.position.x = spaceship.position.x + spaceship.speed * Math.cos(-spaceship.angle);
-    spaceship.position.y = spaceship.position.y + spaceship.speed * Math.sin(-spaceship.angle);
+    spaceship.position.x =
+      spaceship.position.x + spaceship.speed * Math.cos(-spaceship.angle);
+    spaceship.position.y =
+      spaceship.position.y + spaceship.speed * Math.sin(-spaceship.angle);
   }
   if (commands["a"] == true) {
     spaceship.angle -= degToRad(5);
   }
   if (commands["d"] == true) {
     spaceship.angle += degToRad(5);
+  }
+  if(spaceship.position.x > canvas.width){
+    spaceship.position.x = 1
+  }
+  if(spaceship.position.x < 0) {
+    spaceship.position.x = canvas.width - 1
+  }
+  if(spaceship.position.y < 0){
+    spaceship.position.y = canvas.height - 1
+  } 
+  if(spaceship.position.y > canvas.height){
+    spaceship.position.y = 1
+  }
+
+  for(let i = 0; i < bullets.length; i++){
+    bullets[i].posX = bullets[i].posX + spaceship.bulletsSpeed * Math.cos(bullets[i].angle);
+    bullets[i].posY = bullets[i].posY + spaceship.bulletsSpeed * Math.sin(bullets[i].angle);
+    if(bullets[i].posX > canvas.width || bullets[i].posX < 0 || bullets[i].posY < 0 || bullets.posY > canvas.height){
+      bullets.splice(i, 1)
+    }
   }
 }
 
